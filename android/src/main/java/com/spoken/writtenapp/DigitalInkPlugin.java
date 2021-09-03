5f3c606df2f9a6e281901952562ce85cfb0de1e6
Andrew <aslyons001@gmail.com>
2021-09-03 16:21:24 -0400

    java progress

--- android/src/main/java/com/spoken/writtenapp/DigitalInkPlugin.java
+++ android/src/main/java/com/spoken/writtenapp/DigitalInkPlugin.java
@@ -6,17 +6,50 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModel;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognitionModelIdentifier;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizer;
+import com.google.mlkit.vision.digitalink.DigitalInkRecognizerOptions;
+import com.google.mlkit.vision.digitalink.Ink;
+
+import org.json.JSONException;
+
+import java.lang.reflect.Array;
+
 @CapacitorPlugin(name = "DigitalInk")
 public class DigitalInkPlugin extends Plugin {
 
-    private DigitalInk implementation = new DigitalInk();
+    public int[] points = null;
+    public int[] strokes = null;
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void erase(PluginCall call) {
+        points = strokes = null;
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("ok", true);
+        ret.put("msg", "Erased stored stroke and point data.");
+
         call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void logStrokes(PluginCall call) throws JSONException {
+        com.getcapacitor.JSArray xArr = call.getArray("x");
+        com.getcapacitor.JSArray yArr = call.getArray("y");
+        com.getcapacitor.JSArray tArr = call.getArray("t");
+
+        if (tArr.length() != 0) {
+            for (int i  = 0; i < xArr.length(); i++) {
+                float x = (float) xArr.getInt(i);
+                float y = (float) yArr.get(i);
+                long t = tArr.getInt(i);
+
+            }
+        }
+        else {
+
+        }
+
     }
 }
